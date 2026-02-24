21550f127f3ec525fbc2e11446b25b7eb6b913eb
Luis Sanchez <luis010993@gmail.com>
2026-02-24 18:22:42 +0100

    actualizar vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  
-
-  preview: { //Para trabajar con railway
-    allowedHosts: ['impartial-spontaneity-production-36c6.up.railway.app'],
-  },
+  preview: {
+    allowedHosts: ['practical-spirit-production.up.railway.app']
+  }
 })
